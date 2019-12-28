48ee217ec16809b1913e4031f27236ef310d34cd
Abdirashid Awil <rashidawil437@gmail.com>
2019-12-28 21:33:56 +0000

    Updates

---
+++ precache-manifest.2504ab97e99054525ac7c0a564a72323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb9981c5caf0f6c4fd1bce7ccf8679f",
+    "url": "/abaya/index.html"
+  },
+  {
+    "revision": "16c7ed97a369808b7f27",
+    "url": "/abaya/static/css/main.011788d9.chunk.css"
+  },
+  {
+    "revision": "838824a936fce6b1fe72",
+    "url": "/abaya/static/js/2.2ddec0bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/abaya/static/js/2.2ddec0bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16c7ed97a369808b7f27",
+    "url": "/abaya/static/js/main.e5c7624e.chunk.js"
+  },
+  {
+    "revision": "1c4b1848d17d4ab98ae4",
+    "url": "/abaya/static/js/runtime-main.e9b6fcce.js"
+  }
+]);
