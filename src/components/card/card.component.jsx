d22d3b5b83af1c985476c7f006f9fcf98a6b183a
Abdirashid Awil <rashidawil437@gmail.com>
2019-12-29 23:14:56 +0000

    tried adding some styles to cards...needs improvement

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './card.styles.css'
 
 export const Card = (props) => (
-  <div className="card-container">
-    <img className="card-img" alt="book" src={props.book.image} />
-    <h2> {props.book.title} </h2>
-    <h5> {props.book.subtitle} </h5>
+  <div className="card-container card">
+        <div className="card-header">
+      <h5 className="card-title"> {props.book.title} </h5>
+      </div>
+    <img className="card-img-top" alt="book" src={props.book.image} />
+    <div className="card-body">
+    <h5 className="card-subtitle mb-2 text-muted"> {props.book.subtitle} </h5>
+    <p className="card-text"> {props.book.description} </p>
     <h6> {props.book.author} </h6>
+    </div>
+
   </div>  
 )
